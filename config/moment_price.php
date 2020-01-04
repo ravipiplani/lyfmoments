42f2e6eace406d0422952dcab8db6f14d7ca97ad
Ravi Piplani <ravi@kalaage.net>
2020-01-04 17:44:22 +0530

    added currencies

--- config/moment_price.php
+++ config/moment_price.php
@@ -10,5 +10,55 @@ return [
         'currency_symbol' => '$',
         'currency' => 'USD',
         'value' => 1
-    ]
+    ],
+    'CA' => [
+        'currency_symbol' => '$',
+        'currency' => 'CAD',
+        'value' => 1
+    ],
+    'AU' => [
+        'currency_symbol' => '$',
+        'currency' => 'AUD',
+        'value' => 1
+    ],
+    'DE' => [
+        'currency_symbol' => '€',
+        'currency' => 'EUR',
+        'value' => 1
+    ],
+    'RU' => [
+        'currency_symbol' => '₽',
+        'currency' => 'RUB',
+        'value' => 1
+    ],
+    'SG' => [
+        'currency_symbol' => '$',
+        'currency' => 'SGD',
+        'value' => 1
+    ],
+    'ZA' => [
+        'currency_symbol' => 'R',
+        'currency' => 'ZAR',
+        'value' => 1
+    ],
+    'AE' => [
+        'currency_symbol' => 'د.إ',
+        'currency' => 'AED',
+        'value' => 1
+    ],
+    'GB' => [
+        'currency_symbol' => '£',
+        'currency' => 'GBP',
+        'value' => 1
+    ],
+    'CN' => [
+        'currency_symbol' => '¥',
+        'currency' => 'CNY',
+        'value' => 1
+    ],
+    'JP' => [
+        'currency_symbol' => '¥',
+        'currency' => 'JPY',
+        'value' => 1
+    ],
 ];
